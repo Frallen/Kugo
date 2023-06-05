b8e093cc3098ac48ca36be636754a9c58913063c
Vladislav Popov <v.popov@rootmedia.ru>
2023-06-05 21:14:33 +0300

    feat:category min

--- client/pages/catalog/index.vue
+++ client/pages/catalog/index.vue
@@ -2,8 +2,11 @@
   <div class="catalog-head">
     <NuxtImg src="catalog.png"></NuxtImg>
     <div class="container">
-      <h1>Каталог товаров</h1>
+      <h4>Каталог товаров</h4>
     </div>
+  </div>
+  <div class="container">
+    <Categories></Categories>
   </div>
 </template>
 
@@ -16,23 +19,47 @@ definePageMeta({
 
 <style scoped lang="less">
 .catalog-head {
-
   background: linear-gradient(88.23deg, #6F73EE 24.95%, rgba(111, 115, 238, 0) 59.45%);
   border-radius: 10px;
-  margin: 0 1.875em 50px;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  margin: 20px 1.875em 50px;
+  position: relative;
+  height: 200px;
+  .br(10px);
+  overflow: hidden;
+  @media @lg {
+    margin: 20px 1.3em 50px;
+
+  }
+  @media @md {
+    margin: 20px 1em;
+  }
+  @media @sm {
+    margin: 20px 0.5em;
   }
 
-  h1 {
+  img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    transform: translateX(13%);
+    @media @lg {
+      object-fit: cover;
+    }
+  }
+
+  h4 {
+    padding: 70px 0;
     font-weight: 600;
     font-size: 2.188em;
     line-height: 51px;
     color: #fff;
     text-transform: uppercase;
-    z-index: 2;
+    @media @lg {
+      font-size: 1.5em;
+    }
   }
 }
 </style>
